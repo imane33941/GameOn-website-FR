0d5a170dfdc3225684da47be559992d9d44010b4
imane33941 <imane.bichon@icloud.com>
2025-01-03 16:55:18 +0100

    use classList instead of className for editNav function and create closeModal function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,20 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+const modalBground = document.querySelector(".bground");
+const modalButtons = document.querySelectorAll(".modal-btn");
+const formInputs = document.querySelectorAll(".formData");
+const closeModalButton = document.querySelector(".close");
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+modalButtons.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalButton.addEventListener("click", closeModal);
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBground.classList.add("active");
 }
 
+function closeModal() {
+  modalBground.classList.remove("active");
+}
 
+function editNav() {
+  const topNavbar = document.getElementById("myTopnav");
+  topNavbar.classList.toggle("responsive");
+}
